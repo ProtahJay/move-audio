e9434ba17575db71a49df6e91e40b7200c8eda0e
Jason Wilson <protahjay@gmail.com>
2023-06-14 10:56:55 -0400

    added additional logic and exclusion option

--- move-audio.py
+++ move-audio.py
@@ -1,9 +1,13 @@
 import os
 import shutil
 
-def copy_mp3_files(source_path, destination_path):
+def move_mp3_files(source_path, destination_path):
     try:
         for root, dirs, files in os.walk(source_path):
+             # Exclude specific directory
+            if "Prelog" in dirs:
+                dirs.remove("Prelog")
+                
             for file in files:
                 if file.endswith(".mp3"):
                     source_file = os.path.join(root, file)
@@ -16,14 +20,23 @@ def copy_mp3_files(source_path, destination_path):
                         os.makedirs(destination_subdirectories, exist_ok=True)
                     
                     if not os.path.exists(destination_file):
-                        shutil.copy2(source_file, destination_file)
-                        print(f"Copied {source_file} to {destination_file}")
+                        shutil.move(source_file, destination_file)
+                        print(f"Moved {source_file} to {destination_file}")
                     else:
-                        print(f"Destination file {destination_file} already exists. Skipped copying.")
+                        print(f"Destination file {destination_file} already exists. Skipped moving.")
         
-        print(f"Successfully copied files from {source_path} to {destination_path}")
+        print(f"Successfully moved files from {source_path} to {destination_path}")
+
+        # Remove empty subdirectories in the source path
+        for root, dirs, files in os.walk(source_path, topdown=False):
+            for directory in dirs:
+                directory_path = os.path.join(root, directory)
+                if not os.listdir(directory_path):
+                    os.rmdir(directory_path)
+                    print(f"Removed empty directory: {directory_path}")
+
     except Exception as e:
-        print(f"Error copying files: {str(e)}")
+        print(f"Error moving files: {str(e)}")
 
 # Example usage
 source_path = r"D:\move-audio\recorder\D\Recordings"
@@ -32,4 +45,4 @@ destination_path = r"D:\move-audio\storage\audiostore\Vericore\Intake\2023\Prima
 print(f"Source path: {source_path}")
 print(f"Destination path: {destination_path}")
 
-copy_mp3_files(source_path, destination_path)
+move_mp3_files(source_path, destination_path)
